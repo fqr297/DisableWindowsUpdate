1ccfce7be5068236996b6eea8cb74ad44ecba791
Adrian Zaręba <fqr297@protonmail.com>
2018-07-18 04:18:44 +0200

    First

---
+++ DisableWindowsUpdate/Process.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DisableWindowsUpdate
+{
+    class Process
+    {
+        // Creates CMD.exe process and executes given commands 
+        public void RunCmd(string command)
+        {
+            System.Diagnostics.Process.Start("CMD.exe", "/C " + command);
+        }
+
+        // Disables Windows Update through registry edit and Windows Update Service (wuauserv) through sc
+        public void Disable()
+        {
+            RunCmd("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\\Auto Update\" /v AUOptions /t REG_DWORD /d 1 /f");
+            RunCmd("net stop wuauserv");
+            RunCmd("sc config wuauserv start=disabled");
+            Console.WriteLine("Windows Update was successfully disabled!");
+            Console.ReadKey();
+        }
+
+        // Reverse Disable() 
+        public void Enable()
+        {
+            RunCmd("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\\Auto Update\" /v AUOptions /t REG_DWORD /d 3 /f");
+            RunCmd("net start wuauserv");
+            RunCmd("sc config wuauserv start=auto");
+            Console.WriteLine("Windows Update was successfully re-enabled");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DisableWindowsUpdate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DisableWindowsUpdate
+{
+    class Program
+    {
+        //Invoke Menu
+        static void Main(string[] args)
+        {
+            Process ps = new Process();
+            Console.WriteLine("============================");
+            Console.WriteLine("Welcome");
+            Console.WriteLine("============================");
+            Console.WriteLine("(d)isable or (e)nable Windows Update");
+            Console.Write("Choose action: ");
+            string choice = Console.ReadLine();
+            if (choice.Equals("d"))
+            {
+                ps.Disable();
+            }
+            else if (choice.Equals("e"))
+            {
+                ps.Enable();
+            } else
+            {
+                Console.WriteLine("Invalid choice");
+            }
+            Console.WriteLine("Made by FQR, 2018");
+        }
+    }
+}
